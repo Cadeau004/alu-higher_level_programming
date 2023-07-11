52e53e34968bc6c3ee17aefd86d0b0bb917f6894
Cadeau004 <b.ntungane@alustudent.com>
2023-07-11 09:56:23 -0700

    3-infinite_add.py

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -5,5 +5,6 @@ if __name__ == "__main__":
     total = 0
     for i in range(len(sys.argv) - 1):
         total += int(sys.argv[i + 1])
-    print("{}".format(total))
+    print ("{}".format(total))
 
+    :9:1: W391 blank line at end of file
